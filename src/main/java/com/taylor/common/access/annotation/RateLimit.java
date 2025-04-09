1c06eb6f9b522befe5be7dcab6f74e1933889e49
loveCamille <343235900@qq.com>
2025-04-09 14:24:02 +0800

    注解添加Target和Retention

--- src/main/java/com/taylor/common/access/annotation/RateLimit.java
+++ src/main/java/com/taylor/common/access/annotation/RateLimit.java
@@ -1,5 +1,10 @@
 package com.taylor.common.access.annotation;
 
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RateLimit {
 
     /**
