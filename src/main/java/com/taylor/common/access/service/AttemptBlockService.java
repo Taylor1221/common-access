fa93624ce5cde9ff788422b1d71fee9078fdee9b
loveCamille <343235900@qq.com>
2025-04-21 16:21:29 +0800

    新增封禁接口（暂未实现）

--- src/main/java/com/taylor/common/access/service/AttemptBlockService.java
+++ src/main/java/com/taylor/common/access/service/AttemptBlockService.java
@@ -2,17 +2,46 @@ package com.taylor.common.access.service;
 
 import com.taylor.common.access.domain.BlockDTO;
 
+import java.util.List;
+
 
 public interface AttemptBlockService {
 
+    /**
+     * 记录一次失败尝试，可能触发禁止逻辑。
+     * @param key 需要记录的标识
+     */
     void recordAttempt(String key);
 
+    /**
+     * 获取指定键已经尝试的次数
+     * @author loveCamille
+     * @param key 需要获取次数的唯一标识
+     * @return 已经尝试的次数
+    */
     int getAttemptCount(String key);
 
+    /**
+     * 封禁
+     * @author loveCamille
+     * @param blockDTO 封禁信息
+    */
     void block(BlockDTO blockDTO);
 
+    /**
+     * 检查指定键是否已被禁止。
+     * @param key 需要检查的唯一标识
+     * @return 如果被禁止返回 true，否则返回 false
+     */
     boolean isBlocked(String key);
-    
+
+    /**
+     * 重置
+     * @author loveCamille
+     * @param key 需要检查的唯一标识
+    */
     void reset(String key);
+
+    List<BlockDTO> getAllBlockRecords();
 
 }
